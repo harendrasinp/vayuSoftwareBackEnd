3226d46e20b7a6902b0652c7d9b59aeec67969c5
Harendrasinh <haren111990@gmail.com>
2025-07-27 20:12:11 +0530

    add final my domain name to cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const server= express();
 
 server.use(cors({
-  origin: "https://vayu-software.vercel.app/", 
+  origin: "https://vayu-software.vercel.app", 
   credentials: true                
 }));
 server.use(express.json());
